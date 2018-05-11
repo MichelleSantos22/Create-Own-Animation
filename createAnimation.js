30faa4aa743a5862149455ded745810d41432ab1
Jacquelyn M Santos <js92852@srcs.loc>
2018-05-11 10:59:08 -0700

    5/11/18

---
+++ createAnimation.js
@@ -0,0 +1,16 @@
+#container {
+  width: 400px;
+  height: 400px;
+  position: relative;
+  background: white;
+  border-style: solid;
+}
+
+#animation1 {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  background: orange;
+  top: 0px;
+  left: 0px;
+}
