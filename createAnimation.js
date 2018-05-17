4465f5228f5394c8b998160c96df00cb14f7cf41
Jacquelyn M Santos <js92852@srcs.loc>
2018-05-17 12:01:12 -0700

    5/17/2018

--- createAnimation.js
+++ createAnimation.js
@@ -1,12 +1,13 @@
-#container {
-  width: 400px;
-  height: 400px;
-  position: relative;
-  background: white;
-  border-style: solid;
+#animation1 {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  background: orange;
+  top: 0px;
+  left: 0px;
 }
 
-#animation1 {
+#animation2 {
   width: 50px;
   height: 50px;
   position: absolute;
