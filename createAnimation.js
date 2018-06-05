49134152d98e2f633173f9479120fee0f02f4a72
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-06-04 20:43:36 -0700

    6/4/2018

--- createAnimation.js
+++ createAnimation.js
@@ -1,17 +1,21 @@
-/*#animation1 {
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  background: orange;
-  top: 0px;
-  left: 0px;
+var box = document.getElementById("animation2");
+var xpos = 0;
+var ypos = 0;
+var xstep = 1;
+var ystep = 1;
+var id = setInterval(frame, 5);
+
+function frame() {
+    if (xpos > 350 || xpos < 0) {
+        xstep = -xstep;
+    }
+     if (ypos > 350 || ypos < 0) {
+        ystep = -ystep;
+    }
+
+    xpos = xpos + xstep;
+    ypos = ypos + ystep;
+    box.style.top = ypos + 'px';
+    box.style.left = xpos + 'px';
 }
 
-#animation2 {
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  background: orange;
-  top: 0px;
-  left: 0px;
-}*/
