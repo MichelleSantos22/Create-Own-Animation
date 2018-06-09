686a857572675e4dc77ca419bf17cb3cb865a097
MichelleSantos22 <31743215+MichelleSantos22@users.noreply.github.com>
2018-06-08 22:15:40 -0700

    6/8/2018

--- createAnimation.js
+++ createAnimation.js
@@ -1,9 +1,9 @@
 var box = document.getElementById("animation2");
 var xpos = 0;
 var ypos = 0;
-var xstep = 1;
+var xstep = 2;
 var ystep = 1;
-var id = setInterval(frame, 5);
+var id = setInterval(frame, 7);
 
 function frame() {
     if (xpos > 350 || xpos < 0) {
@@ -18,4 +18,3 @@ function frame() {
     box.style.top = ypos + 'px';
     box.style.left = xpos + 'px';
 }
-
